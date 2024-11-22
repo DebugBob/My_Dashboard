2ccd13f25906d20990b56b1b4a61a1c88a84efa0
RebeccaZhu29 <pingbozhu59@gmail.com>
2024-11-22 23:15:15 +1100

    created .env and updated weatherService.ts

--- src/weatherService.ts
+++ src/weatherService.ts
@@ -9,19 +9,24 @@ export interface WeatherData {
   weatherDescription?: string
 }
 
+
 export default class WeatherService {
-  async getWeatherData(coordinate: Coordinate): Promise<WeatherData> {
-    // TODO: error handling (wrap in try catch)
-    // TODO: optional chain
-    // TODO: apiKey should not appear in code
-    const apiKey = "ecc91c393cc10af34ada18f1d4196b29"
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${coordinate.lattitude}&lon=${coordinate.longitude}&appid=${apiKey}&units=metric`
-    const res = await fetch(url)
-    const jsonResponse = await res.json()
-    console.log('jsonResponse=', jsonResponse)
-    return {
-      temperature: jsonResponse?.main?.temp,
-      weatherDescription: jsonResponse?.weather[0]?.main
+
+
+  async getWeatherData(coordinate: Coordinate): Promise<WeatherData | Error> {
+
+    try {
+      const url = `${process.env.WEATHER_API_KEY}/data/2.5/weather?lat=${coordinate.lattitude}&lon=${coordinate.longitude}&appid=${process.env.WEATHER_API_KEY}&units=metric`
+      const res = await fetch(url)
+      const jsonResponse = await res.json()
+      console.log('jsonResponse=', jsonResponse)
+      return {
+        temperature: jsonResponse?.main?.temp,
+        weatherDescription: jsonResponse?.weather?.[0]?.main
+      }
+    } catch (err) {
+      console.log('Error:', err);
+      return err as Error;
     }
   }
 }
