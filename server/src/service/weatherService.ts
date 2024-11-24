c2fcbef9fac3496869f9d29b397300c4beb64aa2
RebeccaZhu29 <pingbozhu59@gmail.com>
2024-11-24 21:18:03 +1100

    fix weather api base url

--- server/src/service/weatherService.ts
+++ server/src/service/weatherService.ts
@@ -14,7 +14,7 @@ class WeatherService {
   async getWeatherData(coordinate: Coordinate): Promise<WeatherData | Error> {
 
     try {
-      const url = `${process.env.WEATHER_API_KEY}/data/2.5/weather?lat=${coordinate.lattitude}&lon=${coordinate.longitude}&appid=${process.env.WEATHER_API_KEY}&units=metric`
+      const url = `${process.env.WEATHER_API_BASE_URL}/data/2.5/weather?lat=${coordinate.lattitude}&lon=${coordinate.longitude}&appid=${process.env.WEATHER_API_KEY}&units=metric`
       const res = await fetch(url)
       const jsonResponse = await res.json()
       console.log('jsonResponse=', jsonResponse)
