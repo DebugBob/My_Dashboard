a4cc689282b99eb3ada6db88f4a38e5124d6a8e6
Mohamed Said <126868682+itstechagain@users.noreply.github.com>
2024-11-28 18:35:06 +1100

    Added instances in schema file manually (#25)

--- server/db/schema.sql
+++ server/db/schema.sql
@@ -3,3 +3,19 @@ DROP DATABASE IF EXISTS todo_db;
 
 -- CREATE DATABASE
 CREATE DATABASE todo_db;
+
+-- Switch to the new database (PostgreSQL)
+\c todo_db
+
+-- For MySQL or other databases, use: USE todo_db;
+
+-- Create the 'todo' table
+CREATE TABLE todo (
+    id SERIAL PRIMARY KEY,         -- Auto-incrementing primary key
+    description VARCHAR(255) NOT NULL -- Non-nullable description column
+);
+
+-- Insert data into the 'todo' table
+INSERT INTO todo (description) VALUES ('Buy groceries');
+INSERT INTO todo (description) VALUES ('Wash the dishes');
+INSERT INTO todo (description) VALUES ('Clean the bathroom');
